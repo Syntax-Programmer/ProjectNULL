5dd6a79331ce40548e819d5b763905931e078b8b
Syntax-Programmer <anand6308anand@gmail.com>
2025-05-31 00:39:17 +0530

    RObust memory setup attempt 2

--- src/common.c
+++ src/common.c
@@ -31,6 +31,69 @@ typedef struct {
 
 static Arena common_arena;
 
+/*
+Some algorithms in this file, such as AddArenaFreeSpot, could be optimized if
+free_spots_arr were kept sorted by offset. However, since arena allocations
+only occur at program startup or during occasional resource loading, these
+inefficiencies are acceptable and do not significantly impact overall
+performance.
+*/
+
+/*
+If this function is used from the start, there will be no worry of having rogue
+free spaces that can be merged.
+*/
+static bool AddArenaFreeSpot(size_t old_offset, size_t old_size);
+
+static bool AddArenaFreeSpot(size_t old_offset, size_t old_size) {
+  // An attempt to merge empty block if they are adjacent.
+  int32_t left = -1, right = -1;
+
+  for (int32_t i = 0; i < common_arena.free_spots_len; i++) {
+    size_t spot_offset = common_arena.free_spots_arr[i].offset;
+    size_t spot_size = common_arena.free_spots_arr[i].size;
+
+    if (spot_offset == old_offset && spot_size == old_size) {
+#ifdef DEBUG
+      fprintf(stderr, "The block is already freed.\n")
+#endif
+          return true;
+    }
+    if (spot_offset + spot_size == old_offset) {
+      left = i;
+    }
+    if (old_offset + old_size == spot_offset) {
+      right = i;
+    }
+  }
+
+  if (left != -1 && right != -1) {
+    common_arena.free_spots_arr[left].size +=
+        old_size + common_arena.free_spots_arr[right].size;
+    common_arena.free_spots_arr[right] =
+        common_arena.free_spots_arr[--common_arena.free_spots_len];
+    return true;
+  } else if (left != -1) {
+    common_arena.free_spots_arr[left].size += old_size;
+    return true;
+  } else if (right != -1) {
+    common_arena.free_spots_arr[right].offset = old_offset;
+    common_arena.free_spots_arr[right].size += old_size;
+    return true;
+  } else if (common_arena.free_spots_len == RESERVED_FREE_SPOTS_ARR_SIZE) {
+#ifdef DEBUG
+    fprintf(stderr, "WARNING: Memory is too fragmented, no further "
+                    "reallocation possible.\n");
+#endif
+    return false;
+  }
+  common_arena.free_spots_arr[common_arena.free_spots_len].offset = old_offset;
+  common_arena.free_spots_arr[common_arena.free_spots_len].size = old_size;
+  common_arena.free_spots_len++;
+
+  return true;
+}
+
 bool common_InitArena() {
   common_arena.mem = malloc(DEFAULT_ARENA_SIZE);
   if (!common_arena.mem) {
@@ -132,19 +195,17 @@ size_t common_ReallocData(size_t original_data_offset, size_t data_size,
 #endif
   }
 
+  /*
+  TODO: This doesn't currently grow the existing array if space is available. It
+  always finds a new location. This is a problem.
+  */
   size_t new_offset;
   if ((new_offset = common_AllocData(new_size)) == (size_t)-1) {
     return new_offset;
   }
-
   memcpy(common_FetchData(new_offset, new_size),
          common_FetchData(original_data_offset, data_size), data_size);
-
-  common_arena.free_spots_arr[common_arena.free_spots_len].offset =
-      original_data_offset;
-  common_arena.free_spots_arr[common_arena.free_spots_len].size = data_size;
-
-  common_arena.free_spots_len++;
+  AddArenaFreeSpot(original_data_offset, data_size);
 
   return new_offset;
 }
